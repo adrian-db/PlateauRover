6de51fcb374fdf0ce7b99adfbaf904fa9bd5b301
Adrian Bigland <a.d.bigland@gmail.com>
2013-05-28 16:26:39 +0100

    Initial Commit

---
+++ PlateauRovers/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PlateauRovers
+//
+//  Created by Adrian Bigland on 28/05/2013.
+//  Copyright (c) 2013 Adrian Bigland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
